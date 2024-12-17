51bcc8ca1dc0561c02e9060ee08fc11c4ce65e25
hieu532004 <hieu532004@gmail.com>
2024-12-17 17:00:29 +0700

    Setup space

---
+++ WorkShops/src/App.tsx
@@ -0,0 +1,13 @@
+import { Video } from "./Components/videoSearch"
+
+function App(): JSX.Element {
+  return (
+    <main className="container mx-auto">
+      <Video />
+    </main>
+
+
+  )
+}
+
+export default App
